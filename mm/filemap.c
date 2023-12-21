2a927fbb33310bd6ebcbe7d68cdcfc2ed0e55e40
YukiFTH <charlottersopp@gmail.com>
2023-12-21 10:44:56 +0900

    mm: Import OEM Changes
    Signed-off-by: YukiFTH <charlottersopp@gmail.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -39,6 +39,10 @@
 #include <linux/psi.h>
 #include "internal.h"
 
+#ifdef CONFIG_SDP
+#include <sdp/cache_cleanup.h>
+#endif
+
 #define CREATE_TRACE_POINTS
 #include <trace/events/filemap.h>
 
@@ -48,6 +52,8 @@
 #include <linux/buffer_head.h> /* for try_to_free_buffers */
 
 #include <asm/mman.h>
+
+int want_old_faultaround_pte = 1;
 
 /*
  * Shared mappings implemented 30.11.1994. It's not fully working yet,
@@ -247,16 +253,23 @@ void __delete_from_page_cache(struct page *page, void *shadow)
 	struct address_space *mapping = page->mapping;
 	int nr = hpage_nr_pages(page);
 
+#ifdef CONFIG_SDP
+	if (mapping_sensitive(mapping))
+		sdp_page_cleanup(page);
+#endif
+
 	trace_mm_filemap_delete_from_page_cache(page);
 	/*
 	 * if we're uptodate, flush out into the cleancache, otherwise
 	 * invalidate any existing cleancache entries.  We can't leave
 	 * stale data around in the cleancache once our page is gone
 	 */
-	if (PageUptodate(page) && PageMappedToDisk(page))
+	if (PageUptodate(page) && PageMappedToDisk(page)) {
+		count_vm_event(PGPGOUTCLEAN);
 		cleancache_put_page(page);
-	else
+	} else {
 		cleancache_invalidate_page(mapping, page);
+	}
 
 	VM_BUG_ON_PAGE(PageTail(page), page);
 	VM_BUG_ON_PAGE(page_mapped(page), page);
@@ -2204,6 +2217,7 @@ static struct file *do_sync_mmap_readahead(struct vm_area_struct *vma,
 {
 	struct file *fpin = NULL;
 	struct address_space *mapping = file->f_mapping;
+	unsigned int ra_pages;
 
 	/* If we don't want any read-ahead, don't bother */
 	if (vma->vm_flags & VM_RAND_READ)
@@ -2233,9 +2247,17 @@ static struct file *do_sync_mmap_readahead(struct vm_area_struct *vma,
 	 * mmap read-around
 	 */
 	fpin = maybe_unlock_mmap_for_io(vma, flags, fpin);
-	ra->start = max_t(long, 0, offset - ra->ra_pages / 2);
-	ra->size = ra->ra_pages;
-	ra->async_size = ra->ra_pages / 4;
+#if CONFIG_MMAP_READAROUND_LIMIT == 0
+	ra_pages = ra->ra_pages;
+#else
+	if (ra->ra_pages > CONFIG_MMAP_READAROUND_LIMIT)
+		ra_pages = CONFIG_MMAP_READAROUND_LIMIT;
+	else
+		ra_pages = ra->ra_pages;
+#endif
+	ra->start = max_t(long, 0, offset - ra_pages / 2);
+	ra->size = ra_pages;
+	ra->async_size = ra_pages / 4;
 	ra_submit(ra, mapping, file);
 	return fpin;
 }
@@ -2446,6 +2468,8 @@ repeat:
 			}
 			goto next;
 		}
+		if (is_migrate_rbin_page(page))
+			goto next;
 
 		head = compound_head(page);
 		if (!page_cache_get_speculative(head))
@@ -2484,6 +2508,14 @@ repeat:
 		if (fe->pte)
 			fe->pte += iter.index - last_pgoff;
 		last_pgoff = iter.index;
+
+		if (want_old_faultaround_pte) {
+			if (fe->address == fe->fault_address)
+				fe->flags &= ~FAULT_FLAG_PREFAULT_OLD;
+			else
+				fe->flags |= FAULT_FLAG_PREFAULT_OLD;
+		}
+
 		if (alloc_set_pte(fe, NULL, page))
 			goto unlock;
 		unlock_page(page);
